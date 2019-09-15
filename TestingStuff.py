dc0066bc79073725f2b4695b42b67d28899bcb2f
Shaishav <s.shah19@share.epsb.ca>
2019-09-15 09:49:18 -0600

    Got Line Of best fit to work, now moving on to error bars.

--- TestingStuff.py
+++ TestingStuff.py
@@ -1,4 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import MaxNLocator
+#### Plot the graph
 
-x = [0,1,2,3,4,5]
-y = [0,0.8,0.9,0.1,-0.8,-1]
+f = pd.read_csv('StockTestFile.csv',
+                    names=['X_val','Y_val'],  # Add own names so that code works better
+                    header=None, # So it doesnt use the values as names
+                    na_values=['no info','.'] # So that values are treated like numbers not strings
+                    )
+x = f['X_val'].astype(float)
+y = f['Y_val'].astype(float)
+#
+graph = plt.figure(1, figsize=(6.4,4.8))
+graph = graph.add_subplot(111)
+graph.scatter(x,y)
+graph.xaxis.set_major_locator(MaxNLocator(integer=True)) # Only integers for the x-axis
+
+#### Line OF Best Fit
+
+m,b = np.polyfit(x,y,1) # the last number is the degree - I think
+
+def line(x):
+    global m,b
+    y = (m*x)+b
+    return y
+
+graph.plot(x, line(x), 'r-')
+
+### Output
+
+plt.show()
