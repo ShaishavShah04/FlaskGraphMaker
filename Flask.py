70311888f889599bb5f20892f59de398ddee893f
Shaishav <shaishav.shah04@gmail.com>
2019-11-17 14:51:39 -0700

    Got the variables to be recognized in other app routes. Now working on making the code actually move onto the next step.

--- Flask.py
+++ Flask.py
@@ -1,48 +1,71 @@
-# FLASK CODE
+# Shaishav Shah #
+# Flask Code Application #
+########################
 
-import os
-from flask import Flask, render_template, redirect, request, url_for, flash
-#
+## Imports
+
+from flask import Flask, render_template, request, redirect, session
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import MaxNLocator
+
+## Intializing flask
+
 app = Flask(__name__)
 app.secret_key = "NoIdea"
 app.config['UPLOAD_FOLDER'] = './Uploads'
 ALLOWED_EXTENSIONS = set(['csv'])
 
-#
+#### Subroutines ####
+
 def allowed_file(filename):
     if filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS:
         return True
     else:
         return False
-#
+
+#### Main Code now ####
 
 @app.route('/')
 def home():
     return render_template("main.html")
 
+
 @app.route('/', methods = ['POST','GET'])
 def upload_file():
+    global title, x_axis, y_axis
     if request.method == 'POST':
-        file = request.files['file']
-        if True == allowed_file(file.filename):
-            file.save(file.filename)
-            print(file)
+        f = request.files['file']
+        if True == allowed_file(f.filename):
+            f.save(f.filename)
+
     if request.method == 'POST':
         if request.method == 'POST':
-            result = request.form
-            print(result)
+            reqform = request.form
+            #
+            session['title'] = reqform['title']
+            session['x_axis'] = reqform['x_axis']
+            session['y_axis'] = reqform['y_axis']
 
     return render_template('uploaded.html')
 
+@app.route('/', methods = ['POST','GET'])
+def making_graph():
+    title = session.get('title', None)
+    x_axis = session.get('x_axis', None)
+    y_axis = session.get('y_axis', None)
 
+    print(title, x_axis, y_axis)
 
+    return render_template('uploaded.html')
 
 
 '''
 @app.route('/instructions')
 def instructions():
     return render_template("instruction.html")
-''' # I dont wanna do this right now, will add later
+'''
 
 
 if __name__ == '__main__':
